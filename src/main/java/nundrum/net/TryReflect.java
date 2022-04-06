7ba8117d8501f27e3c6c7759b524491b0b387f28
Robbie <robbie.huffman@nundrum.net>
2022-04-06 12:05:10 -0400

    TryReflect gets a proper error now

--- src/main/java/nundrum/net/TryReflect.java
+++ src/main/java/nundrum/net/TryReflect.java
@@ -1,21 +1,39 @@
 
 package nundrum.net;
 
+import static java.lang.System.out;
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
 import java.awt.*;
 import java.awt.peer.ComponentPeer;
+import java.awt.peer.*;
 import sun.awt.AWTAccessor.ComponentAccessor;
 import sun.awt.*;
+import sun.awt.X11.*;
 
 public class TryReflect {
 
 	public static void main(String[] args) {
 		Frame frame = new Frame("test");
 
+		frame.setVisible(true);
+		frame.setVisible(false);
+
 		ComponentAccessor acc = AWTAccessor.getComponentAccessor();
 
 		ComponentPeer peer = acc.getPeer(frame);
 
-		long windowID = peer.getShell();
+		Class pc = peer.getClass();
+		try {
+			Method gs = pc.getDeclaredMethod("getShell");
+			Object o = gs.invoke(pc);
+			out.format("Got ID %d", (long) o);
+		} 
+		catch (NoSuchMethodException x) { x.printStackTrace(); }
+		catch (IllegalAccessException x) { x.printStackTrace(); }
+		catch (InvocationTargetException x) { x.printStackTrace(); }
+
+		//long windowID = peer.getShell();
 
 
 
